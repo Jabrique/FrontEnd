7e70ed4c738ab4c9b67b80325f28b5bb1bb12034
Jabrique <ShiroNimex@gmail.com>
2023-03-12 15:45:34 +0700

    latihan tailwind

---
+++ html/tailwind/dist/index.js
@@ -0,0 +1,32 @@
+// hamburger button
+const hamburger = document.querySelector('#hamburger')
+const navMenu = document.querySelector('#nav-menu')
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active')
+    navMenu.classList.toggle('hidden')
+})
+
+//klik selain hamburger bikin hamburger ke close
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target!= navMenu){
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden')
+    }
+})
+
+// navbar scroll
+window.onscroll = function() {
+    const header = document.querySelector('header')
+    const fixednav = header.offsetTop
+    const toTop = document.querySelector('#to-top')
+
+    if(window.pageYOffset > fixednav) {
+        header.classList.add('navbar-fixed')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
+    }else{
+        header.classList.remove('navbar-fixed')
+        toTop.classList.add('hidden')
+    }
+}
