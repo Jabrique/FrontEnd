d29ebaa15a9c34791eb1ecea78f0990ce408037f
Jabrique <ShiroNimex@gmail.com>
2023-05-03 15:00:14 +0700

    proses proyek story app 1

--- html/submission story app/story-app/src/js/index.js
+++ html/submission story app/story-app/src/js/index.js
@@ -3,3 +3,11 @@ import '../sass/main.scss';
 
 // Import javascript file as needed
 import * as bootstrap from 'bootstrap';
+
+document.addEventListener('scroll', (event) => {
+    if (window.scrollY > 0) {
+      document.querySelector('.navbar').classList.add('scrolled');
+    } else {
+      document.querySelector('.navbar').classList.remove('scrolled');
+    }
+});
